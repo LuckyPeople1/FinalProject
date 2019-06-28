419ad54386d0b650ae3beaf082131cafa759a347
jjingbak <qurdoo228@gmail.com>
2019-06-28 18:42:22 +0900

    0628공지사항시작

---
+++ FinalProject/src/main/java/com/dassa/service/NoticeService.java
@@ -0,0 +1,22 @@
+package com.dassa.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+//import com.dassa.dao.NoticeDao;
+import com.dassa.vo.NoticeVO;
+
+@Service("noticeService")
+public class NoticeService {
+//	@Autowired
+//	@Qualifier("noticeDao")
+//	public NoticeDao noticeDao;
+//	
+//	public ArrayList<NoticeVO> noticeList(){
+//		return (ArrayList<NoticeVO>) (noticeDao.noticeList());
+//	}
+}
