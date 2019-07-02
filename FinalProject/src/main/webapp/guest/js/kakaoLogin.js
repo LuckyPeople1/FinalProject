f78a1dbe3f1ab83aeb785dbbe69d8ea15e3c487f
LuckyPeople1 <kyeonghwan2258@gmail.com>
2019-07-02 17:53:12 +0900

    로그인 페이지 css설정 완료

--- FinalProject/src/main/webapp/guest/js/kakaoLogin.js
+++ FinalProject/src/main/webapp/guest/js/kakaoLogin.js
@@ -1,10 +1,10 @@
-src="//developers.kakao.com/sdk/js/kakao.min.js";
 //<![CDATA[
 // 사용할 앱의 JavaScript 키를 설정해 주세요.
-Kakao.init('a44556c982b2f8c93ffc9c39eef6c4bb');  //여기서 아까 발급받은 키 중 javascript키를 사용해준다.
+Kakao.init("a44556c982b2f8c93ffc9c39eef6c4bb");  //여기서 아까 발급받은 키 중 javascript키를 사용해준다.
 // 카카오 로그인 버튼을 생성합니다.
 Kakao.Auth.createLoginButton({
 	container: '#kakao-login-btn',
+	size: 'small',
 	success: function(authObj) {
 		Kakao.API.request({
 			url:'/v1/user/me',
