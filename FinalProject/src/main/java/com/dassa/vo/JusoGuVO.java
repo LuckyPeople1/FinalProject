7ae446b1ed4b9889ecc7dc5919244c4fba86e3a6
unuz <unuz94@gmail.com>
2019-07-04 18:56:46 +0900

    중개사무소, 주소, 추가 2019-07-04 오후 6:56 성윤호

---
+++ FinalProject/src/main/java/com/dassa/vo/JusoGuVO.java
@@ -0,0 +1,27 @@
+package com.dassa.vo;
+
+public class JusoGuVO {
+	private String jusoGuCode;
+	private String jusoGuName;
+	public JusoGuVO() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public JusoGuVO(String jusoGuCode, String jusoGuName) {
+		super();
+		this.jusoGuCode = jusoGuCode;
+		this.jusoGuName = jusoGuName;
+	}
+	public String getJusoGuCode() {
+		return jusoGuCode;
+	}
+	public void setJusoGuCode(String jusoGuCode) {
+		this.jusoGuCode = jusoGuCode;
+	}
+	public String getJusoGuName() {
+		return jusoGuName;
+	}
+	public void setJusoGuName(String jusoGuName) {
+		this.jusoGuName = jusoGuName;
+	}
+}
