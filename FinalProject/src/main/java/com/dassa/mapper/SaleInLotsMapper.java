361f34fbc2a8bd5274880497afb6e5b2f29b39d4
dmmn1156 <dmmn1156@gmail.com>
2019-07-17 19:40:23 +0900

    분양 페이지 거의 다 만듬

--- FinalProject/src/main/java/com/dassa/mapper/SaleInLotsMapper.java
+++ FinalProject/src/main/java/com/dassa/mapper/SaleInLotsMapper.java
@@ -5,11 +5,22 @@ import java.util.ArrayList;
 
 import org.mybatis.spring.annotation.MapperScan;
 
+import com.dassa.vo.SaleInLotsFaqVO;
+import com.dassa.vo.SaleInLotsTremsVO;
 import com.dassa.vo.SaleInLotsVO;
 
 @MapperScan("SaleInLotsMapper")
 public interface SaleInLotsMapper {
+	//분양정보
 	public ArrayList<SaleInLotsVO> saleInLotsInformation(SaleInLotsVO saleInfo) throws Exception;
+	//페이지용 카운터
 	public int saleInLotstotalCount(SaleInLotsVO saleInfo) throws Exception;
+	//분양 상세페이지
 	public SaleInLotsVO saleInLotsDetailInformation(String saleInLotsIDX) throws Exception;
+	//분양용어
+	public ArrayList<SaleInLotsTremsVO> saleInLotsTerms() throws Exception;
+	//분양 자주하는 질문 전체
+	public ArrayList<SaleInLotsFaqVO> saleInLotsFaq() throws Exception;
+	//분양 자주하는 질문 타입
+	public ArrayList<SaleInLotsFaqVO> saleInLotsFaqAjax(SaleInLotsFaqVO sfVo) throws Exception;
 }
