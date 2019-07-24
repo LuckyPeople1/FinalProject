7f78d1dec9decc085449fadf25b7530b18ae80ec
incheonJJang <icjang123@gmail.com>
2019-07-24 11:08:29 +0900

    no message

--- FinalProject/src/main/java/com/dassa/controller/map/MapViewController.java
+++ FinalProject/src/main/java/com/dassa/controller/map/MapViewController.java
@@ -38,7 +38,7 @@ public class MapViewController {
 	
 	@RequestMapping(value="/mapView")
 	public ModelAndView MapView(@RequestParam String shopItemAddr1) throws Exception {
-		System.out.println("shopItemAddr1 : "+shopItemAddr1);
+		
 		ModelAndView mav = new ModelAndView();
 		List<ShopItemImgVO> siiList; // shop_item_tbl
 		ArrayList<ShopItemVO> list; //shop_item_img_tbl	
@@ -201,12 +201,16 @@ public class MapViewController {
 	@ResponseBody
 	@RequestMapping(value="/mapSelectOne")
 	public MapImageData MapSelectOne(String shopItemAddr1) throws Exception {		
+		
+		shopItemAddr1=shopItemAddr1.replace("서울특별시", "서울");
+		System.out.println(shopItemAddr1);
 		ArrayList<ShopItemVO> list;
 		List<ShopItemImgVO> siiList; // shop_item_tbl
 		ArrayList<String> imgList = new ArrayList<String>();
 		Map<String, Object> map = new HashMap<String, Object>();		
 		map.put("shopItemAddr1", shopItemAddr1);
-		list = mapService.mapSelectOne(map);				
+		list = mapService.mapSelectOne(map);
+		System.out.println(list);
 		for(int i=0;i<list.size();i++) {			
 			siiList = shopService.shopItemImgList(list.get(i).getShopItemIdx());
 			String img_path = siiList.get(0).getShopImgPath();		
